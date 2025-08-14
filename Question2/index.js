afb62794e644559c034bb067e58c7519a89bab43
Sukhbat05 <asukhbat27@gmail.com>
2025-08-14 23:46:35 +0800

    home08.14

--- Question2/index.js
+++ Question2/index.js
@@ -1,1 +1,20 @@
+const body = document.querySelector("body");
+const startBtn = document.querySelector("button");
 
+const quizStart = () => {
+  startBtn.remove();
+  const question = document.createElement("h2");
+  body.appendChild(question);
+  new Array(4).fill(0).forEach(() => {
+    const btn = document.createElement("button");
+    body.appendChild(btn);
+
+    const drawQuestion = () => {
+      const answerBtns = document.querySelectorAll("button");
+      question.innerText = "hello"
+    }
+   
+  });
+};
+
+startBtn.addEventListener("click", quizStart);
